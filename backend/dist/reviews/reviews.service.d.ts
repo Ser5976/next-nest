1e3086d96ab850c051f36a741ab967063c153536
Seriy <tsbaranova@bk.ru>
2023-01-23 17:27:58 +0300

    commit

--- backend/dist/reviews/reviews.service.d.ts
+++ backend/dist/reviews/reviews.service.d.ts
@@ -1,3 +1,11 @@
+/// <reference types="mongoose/types/pipelinestage" />
+/// <reference types="mongoose/types/aggregate" />
+/// <reference types="mongoose/types/connection" />
+/// <reference types="mongoose/types/cursor" />
+/// <reference types="mongoose/types/document" />
+/// <reference types="mongoose/types/error" />
+/// <reference types="mongoose/types/mongooseoptions" />
+/// <reference types="mongoose/types/schemaoptions" />
 import { UserModel } from './../user/user.model';
 import { UpdateReviewDto } from './dto/update.review.dto';
 import { ReviewsDto } from './dto/reviews.dto';

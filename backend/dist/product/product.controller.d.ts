1c4711dd87198c726a9b36b7e1b1e4ad268cf381
Seriy <tsbaranova@bk.ru>
2023-02-21 19:07:31 +0300

    copy

--- backend/dist/product/product.controller.d.ts
+++ backend/dist/product/product.controller.d.ts
@@ -1,12 +1,3 @@
-/// <reference types="mongoose/types/pipelinestage" />
-/// <reference types="mongoose/types/aggregate" />
-/// <reference types="mongoose/types/connection" />
-/// <reference types="mongoose/types/cursor" />
-/// <reference types="mongoose/types/document" />
-/// <reference types="mongoose/types/error" />
-/// <reference types="mongoose/types/mongooseoptions" />
-/// <reference types="mongoose/types/schemaoptions" />
-/// <reference types="mongoose" />
 import { SearchDto } from './dto/search.dto';
 import { QueryParametrsDto } from './dto/queryParametrs.dto';
 import { ProductDto } from './dto/product.dto';
